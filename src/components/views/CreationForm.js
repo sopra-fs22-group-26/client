23e76815b92b3f5afde4ba774dd333aa25be1f0d
jdhekh <j.dhekhang@gmail.com>
2022-04-15 13:00:30 +0200

    Updated dashboard and creation form. Task creation via button is now possible (tested with POSTMAN). Styling is still missing. close #57, close #58, close #59, close #100

--- src/components/views/CreationForm.js
+++ src/components/views/CreationForm.js
@@ -1,4 +1,5 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useMemo} from 'react';
+import TextField from '@material-ui/core/TextField';
 import {api, handleError} from 'helpers/api';
 import {Spinner} from 'components/ui/Spinner';
 import {Button} from 'components/ui/Button';
@@ -6,28 +7,116 @@ import {useHistory} from 'react-router-dom';
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import Task from 'models/Task';
+import 'styles/views/CreationForm.scss';
 
 
 
-const FormField = props => {
+const TitleField = props => {
   return (
-    <div className="creation form">
-      <label>
-        {props.label}
-      </label>
+    <div className="creation-form title">
       <input
-      className =""
+      className = "creation-form title-input"
+      placeholder = "Task title"
       value={props.value}
-      onChange={e => props.onChange(e.target.value)}
+      onChange={e => props.onChange(e.target.value)} 
       />
     </div>
   );  
 };
 
+const DescriptionField = props => {
+  return (
+    <div className="creation-form description">
+      <input
+      className = " creation-form description-input"
+      placeholder = "Task description"
+      value={props.value}
+      onChange={e => props.onChange(e.target.value)} 
+      />
+    </div>
+  );  
+};
+
+const DateField = props => {
+  return (
+    <div className="creation-form field">
+      <label className= 'creation-form label'>
+        {props.label}
+      </label>
+      <TextField
+      className = "creation-form input"
+      placeholder = "Select due date.."
+      value = {props.value}
+      id = "date"
+      type = "date"
+      onChange={e => props.onChange(e.target.value)} 
+      />
+    </div>
+  );  
+};
+
+const PriorityField = props => {
+  return (
+    <div className="creation-form field">
+      <label className= 'creation-form label'>
+        {props.label}
+      </label>
+      <select value={props.value} onChange={e => props.onChange(e.target.value)}>
+        <option value="NONE">NONE</option>
+        <option value="LOW">LOW</option>
+        <option value="MEDIUM">MEDIUM</option>
+        <option value="HIGH">HIGH</option>
+      </select>
+    </div>
+  );
+};
+
+const FormField = props => {
+  return (
+    <div className="creation-form field">
+      <label className= 'creation-form label'>
+        {props.label}
+      </label>
+      <input
+      className = "creation-form input"
+      placeholder = "Select.."
+      value={props.value}
+      onChange={e => props.onChange(e.target.value)} 
+      />
+    </div>
+  );  
+};
+
+
+TitleField.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
+
+DescriptionField.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
+
+DateField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
+
 FormField.propTypes = {
   label: PropTypes.string,
   value: PropTypes.string,
+  onChange: PropTypes.func
 };
+
+PriorityField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
+
+
 
 const CreationForm = () => {
   const history = useHistory();
@@ -52,7 +141,7 @@ const CreationForm = () => {
       // Store the token into the local storage.
       //localStorage.setItem('token', user.token);
 
-      // Login successfully worked --> navigate to the route /game in the GameRouter
+      // After succesful creation of a new task navigate to /dashboard
       history.push(`/dashboard`);
     } catch (error) {
       alert(`Something went wrong during the creation: \n${handleError(error)}`);
@@ -60,7 +149,62 @@ const CreationForm = () => {
   };
 
   return (
-    <div>Creation Form</div>
+    <BaseContainer>
+      <div className = "creation-form container">
+      
+        <TitleField 
+          value = {title}
+          onChange = {t => setTitle(t)}
+        />
+      
+      
+        <DescriptionField
+            value = {description}
+            onChange = {d => setDescription(d)}
+        />
+      
+      
+        <DateField
+          label = "Due date:"
+          value = {dueDate}
+          onChange = {dd => setDueDate(dd)}
+        />
+        <PriorityField
+          label="Priority:"
+          value={priority}
+          onChange={p => setPriority(p)}
+        />
+
+        <FormField
+          label="Location:"
+          value={location}
+          onChange={l => setLocation(l)}
+        />
+
+        <FormField
+          label="Estimate:"
+          value={estimate}
+          onChange={e => setEstimate(e)}
+        />
+
+
+      
+      <div className = "creation-form buttons" >
+        <button 
+          className = "creation-form cancel-button"
+          onClick = {() => history.push(`/dashboard`)}
+        >
+          Cancel
+        </button>
+        <button 
+          className = "creation-form save-button"
+          onClick = {() => saveTask()}
+        >
+          Save
+        </button>
+      </div>  
+    </div>
+    </BaseContainer>
   );
 }
 
--- src/components/views/Dashboard.js
+++ src/components/views/Dashboard.js
@@ -1,12 +1,22 @@
 import {useEffect, useState} from 'react';
 import {api, handleError} from 'helpers/api';
-import {CreationButton} from 'components/ui/CreationButton';
+//import {CreationButton} from 'components/ui/CreationButton';
 import {useHistory} from 'react-router-dom';
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
+import 'styles/views/Dashboard.scss';
 
 
 const Dashboard = () => {
+
+const CreationButton = props => (
+  <button
+    {...props}
+    style={{width: props.width, ...props.style}}
+    className = "dashboard create-button">
+    {props.children}
+  </button>
+  );
 
   const history = useHistory();
 
