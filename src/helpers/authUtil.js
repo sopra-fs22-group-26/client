b7de164c509ae85e5a9177db5fe856aa6cd3980d
nicomantzanas <nico.mantzanas@uzh.ch>
2022-05-23 07:02:33 +0200

    added automatic logout to AuthUtil

--- src/helpers/authUtil.js
+++ src/helpers/authUtil.js
@@ -10,6 +10,18 @@ export class AuthUtil {
             localStorage.setItem("token", response.data.token)
             return true
         } catch (err) {
+            if (err.response.status === 401) {
+                const requestBody = JSON.stringify({username});
+                await api.post('/auth/logout', requestBody);
+
+                // Clear local cache and navigate to login page
+                localStorage.removeItem('token');
+                localStorage.removeItem('refreshToken')
+                localStorage.removeItem('id');
+                localStorage.removeItem('name');
+                localStorage.removeItem('username');
+                history.push('/login');
+            }
             return false
         }
     }
